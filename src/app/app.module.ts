c4918b3c5eaa1448228883d428b29e48b2a45cf6
Trisralei <timothee.arnauld1@gmail.com>
2025-02-27 20:19:46 +0100

    :ambulance: fix missing date pipe and missing routerLink

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import {RouterLink, RouterLinkActive, RouterOutlet, Routes} from '@angular/router';
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { provideRouter } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: TaskListComponent },
+  { path: 'add-task', component: TaskFormComponent },
+];
+
+@NgModule({
+  declarations: [AppComponent, TaskListComponent, TaskFormComponent],
+  imports: [
+    BrowserModule, // Doit être importé ici uniquement
+    RouterOutlet,
+    FormsModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  providers: [provideClientHydration(withEventReplay()), provideRouter(routes)],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
